50266c37b2fcaa7c72ca57237fcb17c6790297b4
muhammadnazlinashri <112940962+muhammadnazlinashri@users.noreply.github.com>
2024-05-16 15:34:21 +0700

    test suit

---
+++ Test Suites/TestPageInternshipITCamp.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestPageInternshipITCamp</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3f6456d2-40b0-41e8-a5e1-d05799ea28f0</testSuiteGuid>
+   <testCaseLink>
+      <guid>e98eabf6-2030-48cc-9852-5f49bad0ba30</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PageInternshipITCamp</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
