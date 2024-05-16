614c9a20ce53efd9728e263a879deb9d02e14f70
aldorusno <42965765+aldorusno@users.noreply.github.com>
2024-05-16 17:15:31 +0700

    Fix Naming and Add test Suites

---
+++ Test Suites/Article and Course.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Article and Course</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d839743e-f49c-4a7b-add6-9fab9bfad4f4</testSuiteGuid>
+   <testCaseLink>
+      <guid>8b6e5fdc-4079-47f9-ab49-509bda874498</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Article and Course</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
