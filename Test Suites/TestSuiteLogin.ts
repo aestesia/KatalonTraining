bb74c9411ebbb7650002e339cbf200e2eea94a65
aestesia <timothyhudson23@gmail.com>
2024-05-16 10:19:01 +0700

    test commit

---
+++ Test Suites/TestSuiteLogin.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0e3a17a2-5617-4446-b5cf-409cad27ec9d</testSuiteGuid>
+</TestSuiteEntity>
